ea0682c3a069c51c827230d35a225e4eb38dcda3
Marci Prescott <mjprescott@me.com>
2024-01-28 19:47:24 -0700

    Added main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// A variabale to hold my age
+var myAge = 33;
+// A variable to hold earlyYears
+var earlyYears = 2;
+var earlyYears = 2 * 10.5;
+// console.log(earlyYears);
+// A var holding myAge - 2
+var laterYears = myAge -= 2;
+// console.log(laterYears);
+// Multiply layerYears by 4 (Step 5)
+var laterYears = laterYears *= 4;
+// Step 6. console.log(laterYears);
+// Step 7 add earlyYears and laterYears
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+var myName = 'Marci';
+var myName = myName.toLowerCase();
+console.log(myName);
+// Print name and age in dog years
+console.log(` My name is ${myName} I am
+ ${myAge} years old in human years which is 
+${myAgeInDogYears} years old in dog years`);
+
+
